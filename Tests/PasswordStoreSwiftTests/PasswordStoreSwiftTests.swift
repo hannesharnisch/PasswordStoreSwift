5d935eb1fe84294c8e20b3a14894d671924bb548
Hannes Harnisch <haharnisch@uni-potsdam.de>
2020-05-20 19:03:01 +0200

    changes

--- Tests/PasswordStoreSwiftTests/PasswordStoreSwiftTests.swift
+++ Tests/PasswordStoreSwiftTests/PasswordStoreSwiftTests.swift
@@ -6,7 +6,7 @@ final class PasswordStoreSwiftTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PasswordStoreSwift().text, "Hello, World!")
+        let item = PasswordStore.hasLogIn(for: "www.apple.com")
     }
 
     static var allTests = [
