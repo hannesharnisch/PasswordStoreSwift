a30a3c464716265198f376342251e44689963322
Hannes Harnisch <haharnisch@uni-potsdam.de>
2020-05-20 20:47:38 +0200

    updated protectionlevels

--- Sources/PasswordStoreSwift/PasswordStoreSwift.swift
+++ Sources/PasswordStoreSwift/PasswordStoreSwift.swift
@@ -69,8 +69,8 @@ public class PasswordStore{
     }
 }
 public struct Credentials{
-    var username:String
-    var password:String
+    public var username:String
+    public var password:String
     public init(username:String,password:String){
         self.username = username
         self.password = password
