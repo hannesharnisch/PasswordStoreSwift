1ba6bd3aa09a8b7280e918de3d8baa60dd1327fb
Hannes Harnisch <haharnisch@uni-potsdam.de>
2020-05-20 20:21:00 +0200

    updated protectionlevels

--- Sources/PasswordStoreSwift/PasswordStoreSwift.swift
+++ Sources/PasswordStoreSwift/PasswordStoreSwift.swift
@@ -69,8 +69,8 @@ public class PasswordStore{
     }
 }
 public struct Credentials{
-    var username:String
-    var password:String
+    public var username:String
+    public var password:String
 }
 public enum KeychainError: Error {
     case noPassword
